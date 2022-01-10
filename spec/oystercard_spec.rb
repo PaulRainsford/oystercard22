464901d0fa7f9e9f069d580a75f804842083e75e
Paul Rainsford <paulrainsford69@gmail.com>
2022-01-10 23:41:17 +0000

    Removed surplus tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,9 +8,6 @@ describe Oystercard do
 
     describe '#top_up' do
         
-        it { is_expected.to respond_to(:top_up).with(1).argument }
-       
-
         it 'can top up the balance' do
             expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
         end
@@ -22,8 +19,6 @@ describe Oystercard do
         end
 
     end
-
-    it { is_expected.to respond_to(:deduct).with(1).argument }
 
     it 'deducts an amount from the balance' do
         subject.top_up(20)
